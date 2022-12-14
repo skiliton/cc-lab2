cad30c1752248f5899c8878ae6a8ec6d21654932
ivanrepeta <skiliton221@gmail.com>
2022-12-14 23:14:02 +0200

    Initial commit

---
+++ src/main/kotlin/ua/repeta/cclab2/CcLab2Application.kt
@@ -0,0 +1,11 @@
+package ua.repeta.cclab2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CcLab2Application
+
+fun main(args: Array<String>) {
+    runApplication<CcLab2Application>(*args)
+}
---
+++ src/test/kotlin/ua/repeta/cclab2/CcLab2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.repeta.cclab2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CcLab2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
